055e9dec0a8bbf2550b55fd17b13c5d588b5f4b2
OleksiiKhomenko <47694554+OleksiyKH0240@users.noreply.github.com>
2020-12-24 19:35:45 +0200

    Add files via upload

---
+++ The_lift_kata3/The_lift_kata3.py
@@ -0,0 +1,10 @@
+def main(main_arg):  # todo a normal function
+    x = 19
+    print(main_arg)
+    print(x)
+    print(x)
+    print(x)
+
+
+if __name__ == "__main__":  # todo a worked program
+    main("start program")
