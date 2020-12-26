c9c1c3b3a7d2174d0dbd0f3f51ec6c3cf7529da1
OleksiyKH0240 <homenko0240@gmail.com>
2020-12-26 16:44:23 +0200

    second one commit

--- The_lift_kata3.py
+++ The_lift_kata3.py
@@ -1,9 +1,9 @@
 def main(main_arg):  # todo a normal function
     x = 19
     print(main_arg)
-    print(x + 12)
-    print(x)
-    print(x)
+    print(x + 13)
+    print(x +16)
+    print(x + 18)
 
 
 if __name__ == "__main__":  # todo a worked program
